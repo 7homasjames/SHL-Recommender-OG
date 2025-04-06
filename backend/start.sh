517705cf77319153193d47583af7492b20adb115
Thomas James <7homasjames@gmail.com>
2025-04-06 21:38:32 +0530

    Update start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-uvicorn api:app --host 0.0.0.0 --port 8000
+
+# Install Chrome
+apt-get update && apt-get install -y wget unzip curl gnupg
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt install -y ./google-chrome-stable_current_amd64.deb || apt --fix-broken install -y
+
+# Install Chromedriver
+CHROMEDRIVER_VERSION=$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE)
+wget -O /tmp/chromedriver.zip https://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromedriver_linux64.zip
+unzip /tmp/chromedriver.zip -d /usr/local/bin/
+chmod +x /usr/local/bin/chromedriver
+
+# Start the app
+uvicorn backend.main:app --host 0.0.0.0 --port 8000
+
